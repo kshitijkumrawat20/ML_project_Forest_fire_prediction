7117058ff76911bcf5cb4cdbbb2410924f72c258
kshitij kumrawat <kshitijk146@gmail.com>
2024-03-18 13:31:09 +0530

    application.py

--- application.py
+++ application.py
@@ -1,39 +1,46 @@
-from flask import Flask
-from flask import Flask,request,jsonify,render_template
-import numpy as np 
-import pandas as pd 
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import pickle 
+import pickle
+
 application = Flask(__name__)
 app = application
-# import ridge regressor model and standard scaler pickle 
-ridge_model = pickle.load(open('models/ridge.pkl','rb'))
-standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
 
-# Route for homepage 
+# import ridge regressor model and standard scaler
+ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
+# Route for homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoints():
-    if request.method=='POST':
-        Temperature = float(request.form.get('Temperature'))        
-        RH =float(request.form.get('RH'))        
-        Ws= float(request.form.get('Ws'))        
-        Rain = float(request.form.get('Rain'))        
-        FFMC = float(request.form.get('FFMC'))        
-        DMC = float(request.form.get('DMC'))        
-        ISI = float(request.form.get('ISI'))        
-        Classes = float(request.form.get('Classes'))        
-        Region= float(request.form.get('Region'))
+    if request.method == 'POST':
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
 
-        new_scaled_data = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])         
+        new_scaled_data = standard_scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
         result = ridge_model.predict(new_scaled_data)
 
-        return render_template('index.html',result=result[0])
+        # Determine if the forest is in danger or not
+        if result[0] > 0.5:
+            forest_status = "The Forest is in Danger!"
+        else:
+            forest_status = "The Forest is Safe."
+
+        return render_template('index.html', result=result[0], forest_status=forest_status)
     else:
         return render_template('index.html')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
